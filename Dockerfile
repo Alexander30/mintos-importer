a2b4c44f4258a5b43f22e929080c078ba06f6c6e
Mark Stopka <mark.stopka@perlur.cloud>
2020-02-15 15:40:05 +0100

    Update local Docker development environment (#8)
    - Add .env.local with environment variables
    - Update Dockerfile
    - Add Docker Stack compose file
    - Update docker-entrypoint.sh
    - Update GitHub Actions workflow to load environment variables from
    .env.local

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,12 @@ MAINTAINER "Mark Stopka <mark.stopka@perlur.cloud>"
 ENV SERVICE_NAME "mintos-importer"
 
 RUN yum update -y
-RUN yum install -y python3-alembic
+RUN yum install -y python3-alembic crudini
 RUN yum clean all && rm -rf /var/cache/yum
 
-RUN mkdir /usr/src/mintos-importer
-COPY src/ /usr/src/mintos-importer
+RUN mkdir /usr/local/src/mintos-importer
+COPY src/ /usr/local/src/mintos-importer
 COPY docker/ /
 
-WORKDIR /usr/src/mintos-importer
+WORKDIR /usr/local/src/mintos-importer
 ENTRYPOINT ["/usr/local/bin/docker-entrypoint.sh"]
--- docker/usr/local/bin/docker-entrypoint.sh
+++ docker/usr/local/bin/docker-entrypoint.sh
@@ -1,4 +1,43 @@
 #!/usr/bin/env bash
 set -e
 
+# Check environment variables
+if [ -z ${POSTGRES_USER+x} ]; then
+  echo "WARN: POSTGRES_USER variable is not set!"
+  CRITICAL_VARIABLE_UNSET=TRUE
+fi
+
+if [ -z ${POSTGRES_PASSWORD+x} ]; then
+  echo "WARN: POSTGRES_PASSWORD variable is not set!"
+  CRITICAL_VARIABLE_UNSET=TRUE
+fi
+
+if [ -z ${POSTGRES_SERVER+x} ]; then
+  echo "WARN: POSTGRES_SERVER variable is not set!"
+  CRITICAL_VARIABLE_UNSET=TRUE
+fi
+
+if [ -z ${POSTGRES_PORT+x} ]; then
+  echo "INFO: POSTGRES_PORT variable is not set. Configuring default port 5432."
+  PGSQL_PORT=5432
+fi
+
+if [ -z ${POSTGRES_DB+x} ]; then
+  echo "WARN: POSTGRES_DBN variable is not set!"
+  CRITICAL_VARIABLE_UNSET=TRUE
+fi
+
+if [ -z ${CRITICAL_VARIABLE_UNSET+x} ]; then
+  MINTOS_IMPORTER_ROOT=/usr/local/src/mintos-importer/
+  ALEMBIC_CONF=${MINTOS_IMPORTER_ROOT}alembic.ini
+  SQLALCHEMY_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_SERVER}:${POSTGRES_PORT}/${POSTGRES_DB}"
+  echo "INFO: Configuring alembic.ini"
+  crudini --set ${ALEMBIC_CONF} alembic sqlalchemy.url ${SQLALCHEMY_URL}
+else
+  echo "CRIT: PostgreSQL log-in information are missing!"
+  if ! [ -z ${DEBUG+x} ] || [[ "$@" != "bash" ]]; then
+    exit 1
+  fi
+fi
+
 exec "$@"
