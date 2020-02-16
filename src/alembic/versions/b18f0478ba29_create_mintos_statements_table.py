18ed04b2fa45ae79bcabc5d83329249de62e9fad
Mark Stopka <mark.stopka@perlur.cloud>
2020-02-16 14:45:55 +0100

    Update database migrations and improve GitHub CI

--- src/alembic/versions/b18f0478ba29_create_mintos_statements_table.py
+++ src/alembic/versions/b18f0478ba29_create_mintos_statements_table.py
@@ -8,7 +8,6 @@ Create Date: 2020-02-15 15:27:30.746040
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = 'b18f0478ba29'
 down_revision = None
@@ -17,11 +16,33 @@ depends_on = None
 
 
 def upgrade():
+  statement_processing_states = sa.dialects.postgresql.ENUM(
+    'new',
+    'processing',
+    'processed',
+    'failed',
+    name='statement_processing_state'
+  )
+
+  filetypes = sa.dialects.postgresql.ENUM(
+    'xlsx',
+    name='filetypes'
+  )
+
   op.create_table(
     'mintos_statements',
-    sa.Column('id', sa.Integer, primary_key=True),
-    sa.Column('investment_account_id', sa.Integer, primary_key=True),
-    sa.Column('upload_date_time', sa.DateTime)
+    sa.Column('id', sa.INTEGER, primary_key=True),
+    sa.Column('investment_account_id', sa.INTEGER, primary_key=True),
+    sa.Column('statement_file', sa.dialects.postgresql.BYTEA),
+    sa.Column('statement_file_type', filetypes),
+    sa.Column('upload_timestamp', sa.TIMESTAMP),
+    sa.Column('document_sha256_hash', sa.CHAR(64), unique=True),
+    sa.Column('transactions_total', sa.INTEGER),
+    sa.Column('first_transaction_timestamp', sa.TIMESTAMP),
+    sa.Column('last_transaction_timestamp', sa.TIMESTAMP),
+    sa.Column('processing_state', statement_processing_states),
+    sa.Column('processing_start_timestamp', sa.TIMESTAMP),
+    sa.Column('processing_finish_timestamo', sa.TIMESTAMP)
   )
 
 
