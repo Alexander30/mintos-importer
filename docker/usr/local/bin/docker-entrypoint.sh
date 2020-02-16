c8f7c33d3b989732efb90f114f6da66630d31676
Mark Stopka <mark.stopka@perlur.cloud>
2020-02-16 16:23:30 +0100

    Update docker-entrypoint.sh

--- docker/usr/local/bin/docker-entrypoint.sh
+++ docker/usr/local/bin/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Check environment variables
 if [ -z ${POSTGRES_USER+x} ]; then
-  echo "WARN: POSTGRES_USER variable is not set!"
+  echo "WARN POSTGRES_USER variable is not set!"
   CRITICAL_VARIABLE_UNSET=TRUE
 fi
 
@@ -13,17 +13,17 @@ if [ -z ${POSTGRES_PASSWORD+x} ]; then
 fi
 
 if [ -z ${POSTGRES_SERVER+x} ]; then
-  echo "WARN: POSTGRES_SERVER variable is not set!"
+  echo "WARN POSTGRES_SERVER variable is not set!"
   CRITICAL_VARIABLE_UNSET=TRUE
 fi
 
 if [ -z ${POSTGRES_PORT+x} ]; then
-  echo "INFO: POSTGRES_PORT variable is not set. Configuring default port 5432."
+  echo "INFO POSTGRES_PORT variable is not set. Configuring default port 5432."
   POSTGRES_PORT=5432
 fi
 
 if [ -z ${POSTGRES_DB+x} ]; then
-  echo "WARN: POSTGRES_DBN variable is not set!"
+  echo "WARN POSTGRES_DBN variable is not set!"
   CRITICAL_VARIABLE_UNSET=TRUE
 fi
 
@@ -31,18 +31,26 @@ if [ -z ${CRITICAL_VARIABLE_UNSET+x} ]; then
   MINTOS_IMPORTER_ROOT=/usr/local/src/mintos-importer/
   ALEMBIC_CONF=${MINTOS_IMPORTER_ROOT}alembic.ini
   SQLALCHEMY_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_SERVER}:${POSTGRES_PORT}/${POSTGRES_DB}"
-  echo "INFO: Configuring alembic.ini"
+  echo "INFO Configuring alembic.ini"
   crudini --inplace --set ${ALEMBIC_CONF} alembic sqlalchemy.url ${SQLALCHEMY_URL}
 else
-  echo "CRIT: PostgreSQL log-in information are missing!"
+  echo "CRIT PostgreSQL log-in information are missing!"
   if ! [ -z ${DEBUG+x} ] || [[ "$@" != "bash" ]]; then
     exit 1
   fi
 fi
 
-until ( echo > /dev/tcp/${POSTGRES_SERVER}/${POSTGRES_PORT} 2>&1 /dev/null ); do
-  echo "INFO Waiting for PostgreSQL."
+ERROR_COUNTER=0
+until ( echo 2> /dev/null > /dev/tcp/${POSTGRES_SERVER}/${POSTGRES_PORT} ); do
+  echo "INFO Waiting for PostgreSQL server."
   sleep 1s
+  ((ERROR_COUNTER++))
+  if [[ ${ERROR_COUNTER} -gt 60 ]]; then
+    echo "CRIT PostgreSQL server is not reachable after 60 seconds!"
+    if ! [ -z ${DEBUG+x} ] || [[ "$@" != "bash" ]]; then
+      exit 1
+    fi
+  fi
 done
 
 exec "$@"
