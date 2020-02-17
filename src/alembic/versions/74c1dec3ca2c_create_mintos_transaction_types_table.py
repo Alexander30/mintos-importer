4a9c6c131932ac1140c0ec24109793ed9a751ade
Mark Stopka <mark.stopka@perlur.cloud>
2020-02-17 09:00:59 +0100

    Add migrations (#16)
    - Improve coding style
    - Add mintos_transaction_types table migration

---
+++ src/alembic/versions/74c1dec3ca2c_create_mintos_transaction_types_table.py
@@ -0,0 +1,65 @@
+"""create mintos_transaction_types table
+
+Revision ID: 74c1dec3ca2c
+Revises: b18f0478ba29
+Create Date: 2020-02-17 03:02:32.089929
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '74c1dec3ca2c'
+down_revision = 'b18f0478ba29'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+  mintos_transaction_types = op.create_table(
+    'mintos_transaction_types',
+    sa.Column('id', sa.INTEGER, primary_key=True, nullable=False),
+    sa.Column('name', sa.String, unique=True, nullable=False)
+  )
+
+  op.bulk_insert(mintos_transaction_types,
+    [
+      {'name': 'Deposits'},
+      {'name': 'Withdrawals'},
+      {'name': 'Withdrawal cancelled'},
+      {'name': 'Cashback bonus'},
+      {'name': 'Refer a friend bonus'},
+      {'name': 'Outgoing currency exchange transaction'},
+      {'name': 'Incoming currency exchange transaction'},
+      {'name': 'FX commission'},
+      {'name': 'Investment in loan'},
+      {'name': 'Investment in loan  - Invest & Access'},
+      {'name': 'Loan payment: principal received'},
+      {'name': 'Loan payment: interest received'},
+      {'name': 'Loan agreement amended: principal received'},
+      {'name': 'Loan agreement amended: interest received'},
+      {'name': 'Loan early repayment: principal received'},
+      {'name': 'Loan early repayment: interest received'},
+      {'name': 'Loan agreement terminated: principal received'},
+      {'name': 'Loan agreement terminated: interest received'},
+      {'name': 'Loan buyback: principal received'},
+      {'name': 'Loan buyback: interest received'},
+      {'name': 'Loan buyback: late payment interest received'},
+      {'name': 'Loan agreement extended: principal received'},
+      {'name': 'Loan agreement extended: interest received'},
+      {'name': 'Loan late fees received'},
+      {'name': 'Other: principal received'},
+      {'name': 'Other: interest received'},
+      {'name': 'Other: late payment interest received'},
+      {'name': 'Secondary market transaction'},
+      {'name': 'Secondary market transaction  - Invest & Access'},
+      {'name': 'Discount for secondary market transaction'},
+      {'name': 'Premium for secondary market transaction'},
+      {'name': 'Cumulative repurchases of loan parts'},
+    ]
+  )
+
+
+def downgrade():
+  op.drop_table('mintos_transaction_types')
--- src/alembic/versions/b18f0478ba29_create_mintos_statements_table.py
+++ src/alembic/versions/b18f0478ba29_create_mintos_statements_table.py
@@ -1,7 +1,7 @@
 """create mintos_statements table
 
 Revision ID: b18f0478ba29
-Revises: 
+Revises: b7a70396a181
 Create Date: 2020-02-15 15:27:30.746040
 
 """
@@ -10,37 +10,37 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'b18f0478ba29'
-down_revision = None
+down_revision = 'b7a70396a181'
 branch_labels = None
 depends_on = None
 
+statement_processing_states = sa.dialects.postgresql.ENUM(
+  'new',
+  'processing',
+  'processed',
+  'failed',
+  name='statement_processing_state'
+)
+
+filetypes = sa.dialects.postgresql.ENUM(
+  'xlsx',
+  name='filetypes'
+)
+
 
 def upgrade():
-  statement_processing_states = sa.dialects.postgresql.ENUM(
-    'new',
-    'processing',
-    'processed',
-    'failed',
-    name='statement_processing_state'
-  )
-
-  filetypes = sa.dialects.postgresql.ENUM(
-    'xlsx',
-    name='filetypes'
-  )
-
   op.create_table(
     'mintos_statements',
-    sa.Column('id', sa.INTEGER, primary_key=True),
-    sa.Column('investment_account_id', sa.INTEGER, primary_key=True),
-    sa.Column('statement_file', sa.dialects.postgresql.BYTEA),
-    sa.Column('statement_file_type', filetypes),
-    sa.Column('upload_timestamp', sa.TIMESTAMP),
-    sa.Column('document_sha256_hash', sa.CHAR(64), unique=True),
-    sa.Column('transactions_total', sa.INTEGER),
-    sa.Column('first_transaction_timestamp', sa.TIMESTAMP),
-    sa.Column('last_transaction_timestamp', sa.TIMESTAMP),
-    sa.Column('processing_state', statement_processing_states),
+    sa.Column('id', sa.INTEGER, primary_key=True, nullable=False),
+    sa.Column('investment_account_id', sa.INTEGER, primary_key=True, nullable=False),
+    sa.Column('statement_file', sa.dialects.postgresql.BYTEA, nullable=False),
+    sa.Column('statement_file_type', filetypes, nullable=False),
+    sa.Column('upload_timestamp', sa.TIMESTAMP, nullable=False),
+    sa.Column('document_sha256_hash', sa.CHAR(64), unique=True, nullable=False),
+    sa.Column('transactions_total', sa.INTEGER, nullable=False),
+    sa.Column('first_transaction_timestamp', sa.TIMESTAMP, nullable=False),
+    sa.Column('last_transaction_timestamp', sa.TIMESTAMP, nullable=False),
+    sa.Column('processing_state', statement_processing_states, nullable=False),
     sa.Column('processing_start_timestamp', sa.TIMESTAMP),
     sa.Column('processing_finish_timestamo', sa.TIMESTAMP)
   )
@@ -48,3 +48,5 @@ def upgrade():
 
 def downgrade():
   op.drop_table('mintos_statements')
+  statement_processing_states.drop(op.get_bind())
+  filetypes.drop(op.get_bind())
