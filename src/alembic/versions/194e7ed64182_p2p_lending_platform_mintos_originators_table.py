07a1bdcbc4039e119f37e03b4af12c6f44a678c9
Mark Stopka <mark.stopka@perlur.cloud>
2020-03-08 09:47:23 +0100

    Update migrations

---
+++ src/alembic/versions/194e7ed64182_p2p_lending_platform_mintos_originators_table.py
@@ -0,0 +1,24 @@
+"""create p2p_lending_platform_mintos_originators table
+
+Revision ID: 194e7ed64182
+Revises: 564ed9757855
+Create Date: 2020-03-08 08:22:11.841280
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '194e7ed64182'
+down_revision = '564ed9757855'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ src/alembic/versions/e2d4e0049ce6_p2p_lending_platform_mintos_participations_table.py
@@ -0,0 +1,24 @@
+"""create p2p_lending_platform_mintos_participations table
+
+Revision ID: e2d4e0049ce6
+Revises: 194e7ed64182
+Create Date: 2020-03-08 08:22:33.167666
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e2d4e0049ce6'
+down_revision = '194e7ed64182'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
